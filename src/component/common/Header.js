4e1495e86dbe3a5113ba4b1e17585aaf926aeb5f
jiyun <sprare06@gmail.com>
2022-05-28 21:09:45 +0900

    빌드

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,56 +1,54 @@
-import { NavLink } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header(props) {
-	const active = { color: 'aqua' };
+  const active = { color: "aqua" };
 
-	return (
-		<header className={props.type}>
-			<div className='inner'>
-				<h1>
-					<NavLink activeStyle={active} to='/'>
-						LOGO
-					</NavLink>
-				</h1>
+  return (
+    <header className={props.type}>
+      <div className="inner">
+        <h1>
+          <NavLink to="/">LOGO</NavLink>
+        </h1>
 
-				<ul id='gnb'>
-					<li>
-						<NavLink activeStyle={active} to='/department'>
-							Department
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/community'>
-							Community
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/gallery'>
-							Gallery
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/youtube'>
-							Youtube
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/location'>
-							Location
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/join'>
-							Join
-						</NavLink>
-					</li>
-				</ul>
+        <ul id="gnb">
+          <li>
+            <NavLink activeStyle={active} to="/department">
+              Department
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/community">
+              Community
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/gallery">
+              Gallery
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/youtube">
+              Youtube
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/location">
+              Location
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={active} to="/join">
+              Join
+            </NavLink>
+          </li>
+        </ul>
 
-				<FontAwesomeIcon icon={faBars} />
-			</div>
-		</header>
-	);
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
