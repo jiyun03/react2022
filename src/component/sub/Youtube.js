d126e3c683ec0630870882726778878722b5e053
jiyun <sprare06@gmail.com>
2022-05-29 11:39:38 +0900

    유튜브 기본 스타일링 완료

--- src/component/sub/Youtube.js
+++ src/component/sub/Youtube.js
@@ -1,22 +1,46 @@
-import Layout from '../common/Layout';
-import {useEffect, useState} from 'react';
-import axios from 'axios';
+import Layout from "../common/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Youtube() {
-	const key = 'AIzaSyCCs-4zoiklCU1ygt2QFrB2Jy7nrfJc-dY';
-	const playlist = 'PL4wM-rifmHleEgufghHbslM5lnMBYdz1v';
-	const num = 5;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+  const [vids, setVids] = useState([]);
 
-	useEffect(() => {
-		axios.get(url).then((json) => console.log(json.data.items))
-	}, [])
+  const key = "AIzaSyCCs-4zoiklCU1ygt2QFrB2Jy7nrfJc-dY";
+  const playlist = "PL4wM-rifmHleEgufghHbslM5lnMBYdz1v";
+  const num = 5;
+  const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 
-	return (
-		<Layout name={'Youtube'}>
-			<p>Youtube</p>
-		</Layout>
-	);
+  useEffect(() => {
+    axios.get(url).then((json) => {
+      setVids(json.data.items);
+    });
+  }, []);
+
+  return (
+    <Layout name={"Youtube"}>
+      {vids.map((vid, idx) => {
+        const tit = vid.snippet.title;
+        const desc = vid.snippet.description;
+        const date = vid.snippet.publishedAt;
+
+        return (
+          <article key={idx}>
+            <h2>{tit.length > 20 ? tit.substr(0, 20) + "..." : tit}</h2>
+            <div className="txt">
+              <p>{desc.length > 300 ? desc.substr(0, 300) + "..." : tit}</p>
+              <span>{date.split("T")[0]}</span>
+            </div>
+            <div className="pic">
+              <img
+                src={vid.snippet.thumbnails.standard.url}
+                alt={vid.snippet.title}
+              />
+            </div>
+          </article>
+        );
+      })}
+    </Layout>
+  );
 }
 
 export default Youtube;
