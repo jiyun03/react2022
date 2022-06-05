6a31894073b9118d805a775e573faa38a303c5bc
jiyun <sprare06@gmail.com>
2022-06-05 11:28:22 +0900

    내가 한 form 인증...

--- src/component/sub/Join.js
+++ src/component/sub/Join.js
@@ -1,11 +1,105 @@
-import Layout from '../common/Layout';
+import Layout from "../common/Layout";
+import { useState, useEffect } from "react";
 
 function Join() {
-	return (
-		<Layout name={'Join'}>
-			<p>Join</p>
-		</Layout>
-	);
+  const initVal = {
+    userid: "",
+    email: "",
+  };
+
+  const [Val, setVal] = useState(initVal);
+  const [Err, setErr] = useState({});
+
+  // 순서3 : 인수로 전달되는 값으로 인증처리 해서 에러 객체 값 반환 함수
+  const check = (val) => {
+    const errs = {};
+
+    // userid 체크 항목
+    if (val.userid.length < 5) {
+      errs.userid = "아이디를 5글자 이상 입력하세요";
+    }
+    // email 체크 항목
+    if (val.email.length < 8 || !/@/.test(val.email)) {
+      errs.email = "이메일은 최소 8글자 이상 @를 포함해 주세요.";
+    }
+
+    return errs;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log("name : ", name);
+    console.log("value : ", value);
+    // 객체에서 변수값을 key에 넣을 수가 없음
+    // 객체에서 변수값을 key 값으로 활용하려면 객체안에서 변수명을 대괄호로 묶어줌
+    // setVal({...Val, userid : 현재 입력된 값})
+    setVal({ ...Val, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 순서2 : check 함수를 호출해서 현재 Val state에 담겨 있는 값을 check 함수의 인수로 전달해서 err 객체를 생성해서 반환
+    // 그렇게 반환된 err 객체를 다시 Err state에 옮겨담음
+    setErr(check(Val));
+  };
+
+  return (
+    <Layout name={"Join"}>
+      {/* 순서1 : 전송 버튼을 눌러서 handleSubmit 함수 호출 */}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>회원가입 폼 양식</legend>
+          <table border="1">
+            <caption>회원가입 정보 입력</caption>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="userid">USER ID</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="userid"
+                    name="userid"
+                    placeholder="아이디를 입력하세요"
+                    value={Val.userid}
+                    onChange={handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">
+                  <label htmlFor="email">E-MAIL</label>
+                </th>
+                <td>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="이메일 주소를 입력하세요"
+                    value={Val.email}
+                    onChange={handleChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th colSpan="2">
+                  <input type="reset" value="CANCEL" />
+                  <input type="submit" value="SUBMIT" />
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </fieldset>
+      </form>
+    </Layout>
+  );
 }
 
 export default Join;
+
+/*
+	1. 전송 버튼을 눌러서 handleSubmit 함수 호출 
+	2. check 함수를 호출해서 현재 Val state에 담겨 있는 값을 check 함수의 인수로 전달해서 err 객체를 생성해서 반환 그렇게 반환된 err 객체를 다시 Err state에 옮겨담음
+	3. 인수로 전달되는 값으로 인증처리 해서 에러 객체 값 반환 함수
+*/
