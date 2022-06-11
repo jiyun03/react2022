e218d415d567e98a2fa2c9b726e94797827ee281
jiyun <sprare06@gmail.com>
2022-06-11 11:30:35 +0900

    community 컴포넌트 create, read 작업

--- src/component/sub/Community.js
+++ src/component/sub/Community.js
@@ -1,11 +1,68 @@
-import Layout from '../common/Layout';
+import { useState, useRef, useEffect } from "react";
+import Layout from "../common/Layout";
 
 function Community() {
-	return (
-		<Layout name={'Community'}>
-			<p>Community</p>
-		</Layout>
-	);
+  const input = useRef(null);
+  const textarea = useRef(null);
+
+  const [Posts, setPosts] = useState([]);
+
+  const resetPosts = () => {
+    input.current.value = "";
+    textarea.current.value = "";
+  };
+
+  const createPost = () => {
+    if (
+      input.current.value.trim() === "" ||
+      textarea.current.value.trim() === ""
+    ) {
+      resetPosts();
+      return alert("제목과 본문을 입력하세요");
+    }
+    setPosts([
+      {
+        title: input.current.value,
+        content: textarea.current.value,
+      },
+      ...Posts,
+    ]);
+
+    resetPosts();
+  };
+
+  useEffect(() => {
+    console.log(Posts);
+  }, [Posts]);
+
+  return (
+    <Layout name={"Community"}>
+      <div className="inputBox">
+        <input type="text" placeholder="제목을 입력하세요" ref={input} /> <br />
+        <textarea
+          cols="30"
+          rows="5"
+          placeholder="본문을 입력하세요"
+          ref={textarea}
+        ></textarea>
+        <div className="btnSet">
+          <button onClick={resetPosts}>CANCEL</button>
+          <button onClick={createPost}>WRITE</button>
+        </div>
+      </div>
+
+      <div className="showBox">
+        {Posts.map((post, idx) => {
+          return (
+            <article key={idx}>
+              <h2>{post.title}</h2>
+              <p>{post.content}</p>
+            </article>
+          );
+        })}
+      </div>
+    </Layout>
+  );
 }
 
 export default Community;
