766603f2baa4909620ca7b7409a64ef43925f9d5
jiyun <sprare06@gmail.com>
2022-06-12 12:04:38 +0900

    Anime.js 사용법

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import Anime from "../../asset/anim.js";
+import { useRef } from "react";
 
 function Visual() {
-	return <figure id='visual'>Visual</figure>;
+  const box = useRef();
+  const style = {
+    width: 100,
+    height: 100,
+    backgroundColor: "aqua",
+    position: "absolute",
+    top: 100,
+    left: 100,
+  };
+
+  return (
+    <figure id="visual">
+      <div
+        className="box"
+        style={style}
+        ref={box}
+        onClick={(e) => {
+          new Anime(box.current, {
+            prop: "left",
+            value: 400,
+            duration: 500,
+            callback: () => {
+              new Anime(box.current, {
+                prop: "top",
+                value: 400,
+                duration: 500,
+              });
+            },
+          });
+          /*
+          new Anime(e.target, {
+            // prop : "opacity"도 가능
+            prop: "left",
+            value: 400,
+            duration: 500,
+          });
+          new Anime(window, {
+            prop: "scroll",
+            value: 2000,
+            duration: 1500,
+          });
+					*/
+        }}
+      ></div>
+    </figure>
+  );
 }
 
 export default Visual;
