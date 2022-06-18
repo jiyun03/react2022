ef651eb45fb763c9ff95cb01f52e29577de1dc78
jiyun <sprare06@gmail.com>
2022-06-19 01:41:12 +0900

    버튼 활성화 될 때 패럴랙스

--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -7,6 +7,7 @@ function Pics({ Scrolled, start, base }) {
   return (
     <section id="pics" className="myScroll">
       {/* <p style={position >= 0 ? { left: 100 + position } : null}>FLICKR</p> */}
+      {/* <h3 style={position >= 0 ? { left: 100 + position * 2 } : null}>FLICKR</h3> */}
       <p style={{ left: 100 + position }}>FLICKR</p>
       <h3 style={{ left: 100 + position * 2 }}>FLICKR</h3>
     </section>
