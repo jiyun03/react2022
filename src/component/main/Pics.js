8013dbb870f0b78fb5cd963379bbf06354917e11
jiyun <sprare06@gmail.com>
2022-06-18 11:10:39 +0900

    console 오류 수정,. 하지만 의문은 풀리지 않았다

--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -1,10 +1,14 @@
 function Pics({ Scrolled, start, base }) {
-  const position = Scrolled - start - base;
+  let position = 0;
+  if (start) {
+    position = Scrolled - start - base;
+  }
 
   return (
     <section id="pics" className="myScroll">
       {/* <p style={position >= 0 ? { left: 100 + position } : null}>FLICKR</p> */}
       <p style={{ left: 100 + position }}>FLICKR</p>
+      <h3 style={{ left: 100 + position * 2 }}>FLICKR</h3>
     </section>
   );
 }
