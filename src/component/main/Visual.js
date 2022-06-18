fd33afe5c188d5cfde39b9f8427ebdf66f65822b
jiyun <sprare06@gmail.com>
2022-06-18 11:38:49 +0900

    비쥬얼 컴포넌트 스타일링

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -1,17 +1,41 @@
+import Anime from "../../asset/anim.js";
 import { useRef } from "react";
 
 function Visual() {
-  const box = useRef();
-  const style = {
-    width: 100,
-    height: 100,
-    backgroundColor: "aqua",
-    position: "absolute",
-    top: 100,
-    left: 100,
-  };
+  return (
+    <figure id="visual" className="myScroll">
+      <article id="slider">
+        <ul className="panel">
+          <li className="s1 on">
+            <span>1</span>
+          </li>
+          <li className="s2">
+            <span>2</span>
+          </li>
+          <li className="s3">
+            <span>3</span>
+          </li>
+          <li className="s4">
+            <span>4</span>
+          </li>
+          <li className="s5">
+            <span>5</span>
+          </li>
+        </ul>
 
-  return <figure id="visual" className="myScroll"></figure>;
+        <ul className="navi">
+          <li className="on"></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+
+        <button className="prev"></button>
+        <button className="next"></button>
+      </article>
+    </figure>
+  );
 }
 
 export default Visual;
