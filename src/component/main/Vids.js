86e41fbc031845c8677383e60f25ef61862a6f67
jiyun <sprare06@gmail.com>
2022-06-19 14:33:25 +0900

    Vids 컴포넌트 Swiper 작업진행중

--- src/component/main/Vids.js
+++ src/component/main/Vids.js
@@ -1,9 +1,40 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function Vids() {
   return (
     <section id="vids" className="myScroll">
-      Vids
+      <Swiper
+        navigation={true}
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination]}
+        loop={true}
+        spaceBetween={50}
+        slidesPerView={3}
+        centeredSlides={true}
+      >
+        <SwiperSlide>
+          <div className="inner">1</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">2</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">3</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">4</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">5</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="inner">6</div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
