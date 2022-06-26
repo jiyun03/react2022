a099c32612653a15532887c21e568db922b49dd4
jiyun <sprare06@gmail.com>
2022-06-26 11:10:35 +0900

    갤러리 컴포넌트 찹업

--- src/component/common/Popup.js
+++ src/component/common/Popup.js
@@ -7,7 +7,7 @@ import { useState, forwardRef, useImperativeHandle } from "react";
 
   기본적으로 react는 단방향 데이터 바인딩 (부모 -> 자식)
   자식에서 부모로 데이터를 전달하려면 부모에서 자식 컴포넌트를 useRef로 참조
-  자식에서 리턴되는 값이 있어야 부모에서 자식 컴포넌트를 참조
+  자식에서 리턴되는 값이 있어야 부모에서 자식 컴포넌트를 참조 가능
   forwardRef()는 자식 JSX 요소를 ref에 담아서 부모요소로 리턴 가능
   만약 내보내는 값이 JSX가 아닌 특정 커스텀 객체일 때는 useImperativeHandle를 사용해야한다.
   사용방법은 위 단계와 동일
--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -3,6 +3,7 @@ import { useEffect, useState, useRef } from "react";
 import Masonry from "react-masonry-component";
 
 import Layout from "../common/Layout";
+import Popup from "../common/Popup";
 
 function Gallery() {
   const frame = useRef(null);
@@ -15,16 +16,8 @@ function Gallery() {
   const [EnableClick, setEnableClick] = useState(false);
   const input = useRef(null);
 
-  const interest_type = {
-    type: "interest",
-    count: 50,
-  };
-
-  const search_type = {
-    type: "search",
-    count: 50,
-    tags: "바다",
-  };
+  const pop = useRef(null);
+  const [Index, setIndex] = useState(0);
 
   const getFlickr = async (opt) => {
     const key = "4f2ed95542fa600d1ed1488dd32b341b";
@@ -92,74 +85,91 @@ function Gallery() {
   }, []);
 
   return (
-    <Layout name={"Gallery"}>
-      <button onClick={showInterest}>Interest Gallery</button>
-      <div className="searchBox">
-        <input
-          type="text"
-          ref={input}
-          onKeyUp={(e) => {
-            // if (e.keyCode === "13") {
-            //   console.log("keycode");
-            // }
+    <>
+      <Layout name={"Gallery"}>
+        <button onClick={showInterest}>Interest Gallery</button>
+        <div className="searchBox">
+          <input
+            type="text"
+            ref={input}
+            onKeyUp={(e) => {
+              // if (e.keyCode === "13") {
+              //   console.log("keycode");
+              // }
 
-            if (e.key === "Enter") showSearch();
-          }}
-        />
-        <button onClick={showSearch}>search</button>
-      </div>
-      {Loading && (
-        <img
-          className="loading"
-          src={process.env.PUBLIC_URL + "/img/loading.gif"}
-        />
-      )}
-      <article ref={frame}>
-        <Masonry elementType={"ul"} options={masonryOption}>
-          {Items.map((item) => {
-            return (
-              <li key={item.id}>
-                <div className="inner">
-                  <div className="pic">
-                    <img
-                      src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-                      alt={item.title}
-                    />
-                  </div>
-                  <h2>{item.title}</h2>
-                  <div className="profile">
-                    <img
-                      src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
-                      alt={item.owner}
-                      onError={(e) =>
-                        e.target.setAttribute(
-                          "src",
-                          "https://www.flickr.com/images/buddyicon.gif"
-                        )
-                      }
-                    />
-                    <span
-                      onClick={(e) => {
-                        if (!EnableClick) return;
-                        setEnableClick(false);
-                        frame.current.classList.remove("on");
-                        getFlickr({
-                          type: "user",
-                          count: 50,
-                          user: e.target.innerText,
-                        });
+              if (e.key === "Enter") showSearch();
+            }}
+          />
+          <button onClick={showSearch}>search</button>
+        </div>
+        {Loading && (
+          <img
+            className="loading"
+            src={process.env.PUBLIC_URL + "/img/loading.gif"}
+          />
+        )}
+        <article ref={frame}>
+          <Masonry elementType={"ul"} options={masonryOption}>
+            {Items.map((item, idx) => {
+              return (
+                <li key={item.id}>
+                  <div className="inner">
+                    <div
+                      className="pic"
+                      onClick={() => {
+                        setIndex(idx);
+                        pop.current.open();
                       }}
                     >
-                      {item.owner}
-                    </span>
+                      <img
+                        src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+                        alt={item.title}
+                      />
+                    </div>
+                    <h2>{item.title}</h2>
+                    <div className="profile">
+                      <img
+                        src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+                        alt={item.owner}
+                        onError={(e) =>
+                          e.target.setAttribute(
+                            "src",
+                            "https://www.flickr.com/images/buddyicon.gif"
+                          )
+                        }
+                      />
+                      <span
+                        onClick={(e) => {
+                          if (!EnableClick) return;
+                          setEnableClick(false);
+                          frame.current.classList.remove("on");
+                          getFlickr({
+                            type: "user",
+                            count: 50,
+                            user: e.target.innerText,
+                          });
+                        }}
+                      >
+                        {item.owner}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </li>
-            );
-          })}
-        </Masonry>
-      </article>
-    </Layout>
+                </li>
+              );
+            })}
+          </Masonry>
+        </article>
+      </Layout>
+
+      <Popup ref={pop}>
+        {Items.length !== 0 && (
+          <img
+            src={`https://live.staticflickr.com/${Items[Index].server}/${Items[Index].id}_${Items[Index].secret}_b.jpg`}
+            alt={Items.title}
+          />
+        )}
+      </Popup>
+    </>
   );
 }
 
