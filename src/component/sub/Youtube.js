c7f6cf890be50ae32573668672c4540a7da3826f
jiyun <sprare06@gmail.com>
2022-07-03 05:13:39 +0900

    유튜브 수정 app으로 옮겻으니가

--- src/component/sub/Youtube.js
+++ src/component/sub/Youtube.js
@@ -1,39 +1,39 @@
 import Layout from "../common/Layout";
-import { useEffect, useState, useRef } from "react";
-import axios from "axios";
+import { useState, useRef } from "react";
+// import axios from "axios";
 import Popup from "../common/Popup";
 
-import { useSelector, useDispatch } from "react-redux";
-import { setYoutube } from "../../redux/action";
+import { useSelector } from "react-redux";
+// import { setYoutube } from "../../redux/action";
 
 function Youtube() {
   // const [Vids, setVids] = useState([]);
   const [Index, setIndex] = useState(0);
   const pop = useRef(null);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const Vids = useSelector((store) => store.youtubeReducer.youtube);
 
-  const fetchYoutube = async () => {
-    const key = "AIzaSyCCs-4zoiklCU1ygt2QFrB2Jy7nrfJc-dY";
-    const playlist = "PL4wM-rifmHleEgufghHbslM5lnMBYdz1v";
-    const num = 5;
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+  // const fetchYoutube = async () => {
+  //   const key = "AIzaSyCCs-4zoiklCU1ygt2QFrB2Jy7nrfJc-dY";
+  //   const playlist = "PL4wM-rifmHleEgufghHbslM5lnMBYdz1v";
+  //   const num = 5;
+  //   const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 
-    await axios.get(url).then((json) => {
-      // const action = setYoutube(json.data.items);
-      // {type: 'SET_YOUTUBE', payload: json.data.items}
-      // dispatch(action);
-      dispatch(setYoutube(json.data.items));
-    });
-  };
+  //   await axios.get(url).then((json) => {
+  //     // const action = setYoutube(json.data.items);
+  //     // {type: 'SET_YOUTUBE', payload: json.data.items}
+  //     // dispatch(action);
+  //     dispatch(setYoutube(json.data.items));
+  //   });
+  // };
 
   const handlePopup = (index) => {
     setIndex(index);
     pop.current.open();
   };
 
-  useEffect(fetchYoutube, []);
+  // useEffect(fetchYoutube, []);
 
   return (
     <>
