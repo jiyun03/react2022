d71c65d888281f39debe1b4d583af9285a275493
jiyun <sprare06@gmail.com>
2022-07-03 11:29:25 +0900

    store.js에 saga미들웨어 설정

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSage from "./saga";
+
+const SagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(SagaMiddleware));
+SagaMiddleware.run(rootSage);
 
 // store 공간을 생성한 다음 전달 된 reducer를 store에 저장해서 내보냄
-const store = createStore(reducers);
+// const store = createStore(reducers);
 export default store;
