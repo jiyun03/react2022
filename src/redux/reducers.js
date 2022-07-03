65d06c8339cb4f514cf33e0072061ce6cdd7c940
jiyun <sprare06@gmail.com>
2022-07-03 11:18:52 +0900

    flickr 리듀서 추가

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -66,7 +66,24 @@ const youtubeReducer = (state = { youtube: [] }, action) => {
   }
 };
 
+const flickrReducer = (state = { flickr: [] }, action) => {
+  switch (action.type) {
+    case "FLICKR_START":
+      return { ...state };
+
+    case "FLICKR_SUCCESS":
+      return { ...state, flickr: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 //전달된 각각의 reducer를 하나로 합쳐서 반환
-const reducers = combineReducers({ memberReducer, youtubeReducer });
+const reducers = combineReducers({
+  memberReducer,
+  youtubeReducer,
+  flickrReducer,
+});
 
 export default reducers;
