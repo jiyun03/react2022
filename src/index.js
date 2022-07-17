0b4e3ae89e1553adbbb143c024cb3d3b836cfb16
jiyun <sprare06@gmail.com>
2022-07-17 12:03:05 +0900

    slice로 부터 전달온 reducer를 store에 저장후 APP 컴포넌트에 전달

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import store from "./redux/store";
+import { configureStore } from "@reduxjs/toolkit";
+import memberReducer from "./redux/memberSlice";
+import youtubeReducer from "./redux/youtubeSlice";
+
+const store = configureStore({
+  reducer: {
+    member: memberReducer,
+    youtube: youtubeReducer,
+  },
+});
 
 ReactDOM.render(
   <HashRouter>
